d9d04814678a2dab977811f34d7cbed208c33477
田中元気 <cavacan@Cavacan-MBP.local>
2025-01-02 14:45:30 +0900

    apply scss to Home

--- app/controllers/finance_controller.rb
+++ app/controllers/finance_controller.rb
@@ -10,17 +10,27 @@ class FinanceController < ApplicationController
   def upload
     if params[:csv_file].present?
       CSV.foreach(params[:csv_file].path, headers: true) do |row|
+        open_price = row['Open Price'].to_f
+        close_price = row['Close Price'].to_f
+        commission = row['Commission'].to_f
+
+        if row['Type'].downcase == 'buy'
+          tpl = close_price - open_price + commission
+        else
+          tpl = open_price - close_price + commission
+        end
+
         Trade.create!(
           platform: params[:platform],
           open_time: row['Open Time'],
           trade_type: row['Type'],
           trade_size: row['Size'],
           trade_item: row['Item'],
-          open_price: row['Open Price'],
+          open_price: open_price,
           close_time: row['Close Time'],
-          close_price: row['Close Price'],
-          commission: row['Commission'],
-          trade_pl: row['Trade P/L']
+          close_price: close_price,
+          commission: commission,
+          trade_pl: tpl
         )
       end
       redirect_to exness_path, notice: 'Complite CSV.'
